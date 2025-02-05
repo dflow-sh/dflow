4e2e3bce582d73702251f7f67096e4e48e19ca65
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2025-02-05 16:25:49 +0530

    add: ports config

--- src/lib/dokku/index.ts
+++ src/lib/dokku/index.ts
@@ -1,32 +1,33 @@
-import { create } from './apps/create';
-import { installed } from './plugin/installed';
-import { list } from './plugin/list';
-import { logs } from './apps/logs';
-import { list as appList } from './apps/list';
-import { listVars } from './config/listVars';
-import { set } from './config/set';
-import { destroy } from './apps/destroy';
-import { unset } from './config/unset';
-import { destroy as destroyDb } from './plugin/database/destroy';
-import { info } from './plugin/database/info';
-import { infoVersion } from './plugin/database/infoVersion';
-import { logs as databaseLogs } from './plugin/database/logs';
-import { list as databaseList } from './plugin/database/list';
-import { links as databaseLinks } from './plugin/database/links';
-import { link } from './plugin/database/link';
-import { unlink } from './plugin/database/unlink';
-import { create as createDatabase } from './plugin/database/create';
-import { proxyPorts } from './proxy/ports';
-import { proxyPortsAdd } from './proxy/portsAdd';
-import { proxyPortsRemove } from './proxy/portsRemove';
-import { restart } from './process/restart';
-import { report } from './domains/report';
-import { set as domainsSet } from './domains/set';
-import { remove } from './domains/remove';
-import { add } from './domains/add';
-import { sync } from './git/sync';
-import { unlock } from './git/unlock';
-import { auth } from './git/auth';
+import { create } from './apps/create'
+import { destroy } from './apps/destroy'
+import { list as appList } from './apps/list'
+import { logs } from './apps/logs'
+import { listVars } from './config/listVars'
+import { set } from './config/set'
+import { unset } from './config/unset'
+import { add } from './domains/add'
+import { remove } from './domains/remove'
+import { report } from './domains/report'
+import { set as domainsSet } from './domains/set'
+import { auth } from './git/auth'
+import { sync } from './git/sync'
+import { unlock } from './git/unlock'
+import { create as createDatabase } from './plugin/database/create'
+import { destroy as destroyDb } from './plugin/database/destroy'
+import { info } from './plugin/database/info'
+import { infoVersion } from './plugin/database/infoVersion'
+import { link } from './plugin/database/link'
+import { links as databaseLinks } from './plugin/database/links'
+import { list as databaseList } from './plugin/database/list'
+import { logs as databaseLogs } from './plugin/database/logs'
+import { unlink } from './plugin/database/unlink'
+import { installed } from './plugin/installed'
+import { list } from './plugin/list'
+import { portsAdd } from './ports/add'
+import { portsList } from './ports/list'
+import { portsRemove } from './ports/remove'
+import { portsSet } from './ports/set'
+import { restart } from './process/restart'
 
 export const dokku = {
   apps: { create, logs, destroy, list: appList },
@@ -43,10 +44,11 @@ export const dokku = {
     link,
     unlink,
   },
-  proxy: {
-    ports: proxyPorts,
-    portsAdd: proxyPortsAdd,
-    portsRemove: proxyPortsRemove,
+  ports: {
+    list: portsList,
+    set: portsSet,
+    add: portsAdd,
+    remove: portsRemove,
   },
   process: {
     restart,
@@ -62,4 +64,4 @@ export const dokku = {
     unlock,
     auth,
   },
-};
+}
