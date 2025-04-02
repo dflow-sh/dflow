896909e96a77d79eb1a95bee3665bb5ee6870dec
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2025-04-02 18:26:40 +0530

    chore: updated network tab ui

--- src/components/servers/monitoring/MonitoringTabs.tsx
+++ src/components/servers/monitoring/MonitoringTabs.tsx
@@ -5,6 +5,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import CPUTab from './CPUTab'
 import DiskTab from './DiskTab'
 import MemoryTab from './MemoryTab'
+import NetworkTab from './NetworkTab'
 import OverviewTab from './OverviewTab'
 
 const MonitoringTabs = ({
@@ -59,14 +60,6 @@ const MonitoringTabs = ({
 }) => {
   const { overview, detailed } = dashboardMetrics
 
-  // Define disk colors
-  const diskColors = [
-    'hsl(var(--chart-1))',
-    'hsl(var(--chart-2))',
-    'hsl(var(--chart-3))',
-    'hsl(var(--chart-4))',
-  ]
-
   return (
     <Tabs defaultValue='overview' className='mt-12 space-y-4'>
       <TabsList>
@@ -99,7 +92,7 @@ const MonitoringTabs = ({
 
       {/* Network Tab */}
       <TabsContent value='network' className='space-y-4'>
-        {/* <NetworkTab {...detailed} /> */}
+        <NetworkTab {...detailed} />
       </TabsContent>
     </Tabs>
   )
