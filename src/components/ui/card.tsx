b819007b8ed1e93ff31808ac4ecbaff28c0d943b
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2025-04-02 22:32:54 +0530

    chore: updated the card colors

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      'rounded-xl border bg-card text-card-foreground shadow transition-colors duration-300 hover:border-muted-foreground/50',
+      'rounded-xl border bg-muted/30 text-card-foreground shadow transition-colors duration-300 hover:border-muted-foreground/50',
       className,
     )}
     {...props}
