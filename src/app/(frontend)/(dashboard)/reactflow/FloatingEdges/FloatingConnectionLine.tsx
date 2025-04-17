40624cec8bfcd948010d317e1d8ede8b4db0e59e
jagadeesh507 <jagadeeshm778@gmail.com>
2025-04-17 09:05:57 +0530

    chore: Floating edges

---
+++ src/app/(frontend)/(dashboard)/reactflow/FloatingEdges/FloatingConnectionLine.tsx
@@ -0,0 +1,73 @@
+import { Node, Position, getBezierPath } from '@xyflow/react'
+import { JSX } from 'react'
+
+import { getEdgeParams } from './utils'
+
+interface FloatingConnectionLineProps {
+  toX: number
+  toY: number
+  fromPosition: Position
+  toPosition: Position
+  fromNode: Node | null
+}
+
+const FloatingConnectionLine = ({
+  toX,
+  toY,
+  fromPosition,
+  toPosition,
+  fromNode,
+}: FloatingConnectionLineProps): JSX.Element | null => {
+  if (!fromNode) {
+    return null
+  }
+
+  // Mock target node at the cursor position
+  const targetNode: Partial<Node> = {
+    id: 'connection-target',
+    measured: {
+      width: 1,
+      height: 1,
+    },
+    //@ts-ignore
+    internals: {
+      positionAbsolute: { x: toX, y: toY },
+    },
+  }
+  const { sx, sy, tx, ty, sourcePos, targetPos } = getEdgeParams(
+    //@ts-ignore
+    fromNode,
+    targetNode as Node,
+  )
+
+  const [edgePath] = getBezierPath({
+    sourceX: sx,
+    sourceY: sy,
+    sourcePosition: sourcePos || fromPosition,
+    targetPosition: targetPos || toPosition,
+    targetX: tx || toX,
+    targetY: ty || toY,
+  })
+
+  return (
+    <g>
+      <path
+        fill='none'
+        stroke='#222'
+        strokeWidth={1.5}
+        className='animated'
+        d={edgePath}
+      />
+      <circle
+        cx={tx || toX}
+        cy={ty || toY}
+        fill='#fff'
+        r={3}
+        stroke='#222'
+        strokeWidth={1.5}
+      />
+    </g>
+  )
+}
+
+export default FloatingConnectionLine
