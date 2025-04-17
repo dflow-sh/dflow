77db2fb992f2257d910c2c56bb135d66ef0ec027
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2025-04-17 15:34:37 +0530

    chore: skeleton loading for dashboard

--- src/app/(frontend)/(dashboard)/dashboard/page.tsx
+++ src/app/(frontend)/(dashboard)/dashboard/page.tsx
@@ -2,94 +2,85 @@ import LayoutClient from '../layout.client'
 import configPromise from '@payload-config'
 import { Folder } from 'lucide-react'
 import { getPayload } from 'payload'
-import { Suspense } from 'react'
+import { Suspense, use } from 'react'
 
 import { ProjectCard } from '@/components/ProjectCard'
 import ServerTerminal from '@/components/ServerTerminal'
 import CreateProject from '@/components/project/CreateProject'
+import { DashboardSkeleton } from '@/components/skeletons/DashboardSkeletons'
 import { Service } from '@/payload-types'
 
-import DashboardLoading from './DashboardLoading'
+const SuspendedDashboard = () => {
+  const payload = use(getPayload({ config: configPromise }))
 
-const SuspendedTerminal = async () => {
-  const payload = await getPayload({ config: configPromise })
-  const { docs: servers } = await payload.find({
-    collection: 'servers',
-    pagination: false,
-    select: {
-      name: true,
-    },
-  })
+  const [serversRes, projectsRes] = use(
+    Promise.all([
+      payload.find({
+        collection: 'servers',
+        pagination: false,
+      }),
+      payload.find({
+        collection: 'projects',
+        pagination: false,
+      }),
+    ]),
+  )
 
-  return <ServerTerminal servers={servers} />
-}
-
-const SuspendedPage = async () => {
-  const payload = await getPayload({ config: configPromise })
-  const { docs: projects } = await payload.find({
-    collection: 'projects',
-    pagination: false,
-  })
-
-  const { docs: servers } = await payload.find({
-    collection: 'servers',
-    pagination: false,
-  })
+  const servers = serversRes.docs
+  const projects = projectsRes.docs
 
   return (
-    <section className='space-y-6'>
-      <div className='flex items-center justify-between'>
-        <div className='inline-flex items-center gap-1.5 text-2xl font-semibold'>
-          <Folder />
-          Projects
-        </div>
-
-        <CreateProject servers={servers} />
-      </div>
-
-      {projects.length ? (
-        <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
-          {projects.map((project, index) => {
-            const services = (project?.services?.docs ?? []) as Service[]
-            return (
-              <ProjectCard
-                key={index}
-                project={project}
-                servers={servers}
-                services={services}
-              />
-            )
-          })}
-        </div>
-      ) : (
-        <section className='grid min-h-[calc(100vh-40vh)] w-full place-items-center'>
-          <div className='relative mx-auto w-full max-w-lg rounded-lg border border-slate-800 bg-gradient-to-tr from-slate-800 to-slate-900 px-8 pb-6 pt-8 text-center'>
-            <h4 className='inline-flex bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 bg-clip-text text-2xl font-bold text-transparent'>
-              No Projects Found
-            </h4>
-            <p className='text-cq-text-secondary text-pretty pt-1'>
-              You don't have any projects yet. Start by creating a new project
-            </p>
+    <>
+      <section className='space-y-6'>
+        <div className='flex items-center justify-between'>
+          <div className='inline-flex items-center gap-1.5 text-2xl font-semibold'>
+            <Folder />
+            Projects
           </div>
-        </section>
-      )}
-    </section>
+
+          <CreateProject servers={servers} />
+        </div>
+
+        {projects.length ? (
+          <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
+            {projects.map((project, index) => {
+              const services = (project?.services?.docs ?? []) as Service[]
+              return (
+                <ProjectCard
+                  key={index}
+                  project={project}
+                  servers={servers}
+                  services={services}
+                />
+              )
+            })}
+          </div>
+        ) : (
+          <section className='grid min-h-[calc(100vh-40vh)] w-full place-items-center'>
+            <div className='relative mx-auto w-full max-w-lg rounded-lg border border-slate-800 bg-gradient-to-tr from-slate-800 to-slate-900 px-8 pb-6 pt-8 text-center'>
+              <h4 className='inline-flex bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 bg-clip-text text-2xl font-bold text-transparent'>
+                No Projects Found
+              </h4>
+              <p className='text-cq-text-secondary text-pretty pt-1'>
+                You don't have any projects yet. Start by creating a new project
+              </p>
+            </div>
+          </section>
+        )}
+      </section>
+
+      <ServerTerminal servers={servers} />
+    </>
   )
 }
 
 const DashboardPage = () => {
   return (
-    <>
-      <Suspense fallback={<DashboardLoading />}>
-        <LayoutClient>
-          <SuspendedPage />
-        </LayoutClient>
+    <LayoutClient>
+      <Suspense fallback={<DashboardSkeleton />}>
+        <SuspendedDashboard />
       </Suspense>
-
-      <Suspense>
-        <SuspendedTerminal />
-      </Suspense>
-    </>
+    </LayoutClient>
   )
 }
 
