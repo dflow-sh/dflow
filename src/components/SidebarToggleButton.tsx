b81b399da9ce715f421d2f1f308971b26ef92850
lokesh mvs <lokeshmvs21@gmail.com>
2025-04-22 08:01:34 +0530

    chore: Changed the sidebar docs toggle button

--- src/components/SidebarToggleButton.tsx
+++ src/components/SidebarToggleButton.tsx
@@ -5,11 +5,15 @@ import { useSidebarDocs } from '@/providers/SidebarDocsProvider'
 import { Button } from './ui/button'
 
 const SidebarToggleButton = ({ slug }: { slug: string }) => {
-  const { openWith, isOpen } = useSidebarDocs()
+  const { openWith } = useSidebarDocs()
 
   return (
-    <Button onClick={() => openWith(slug)} variant='outline' size='sm'>
-      {isOpen ? 'Hide Docs' : 'Show Docs'}
+    <Button
+      onClick={() => openWith(slug)}
+      variant='link'
+      size='sm'
+      className='text-sm text-primary'>
+      info
     </Button>
   )
 }
