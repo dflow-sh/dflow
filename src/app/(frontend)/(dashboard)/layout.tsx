24026e28ea68bd3586e97fc1874c64a8c6d61351
jagadeesh507 <jagadeeshm778@gmail.com>
2025-04-29 19:22:26 +0530

    chore: layout update in template

--- src/app/(frontend)/(dashboard)/layout.tsx
+++ src/app/(frontend)/(dashboard)/layout.tsx
@@ -41,45 +41,47 @@ const DashboardLayoutInner = () => {
   if (!user.onboarded && totalUsers === 0) redirect('/onboarding')
 
   return (
-    <div className='sticky top-0 z-50 mx-auto flex w-full max-w-6xl items-center justify-between bg-background p-4'>
-      <div className='flex min-h-9 items-center gap-2 text-2xl font-semibold'>
-        <Link href={`/dashboard`} className='flex items-center gap-1'>
-          <Image
-            src='/images/dflow-no-bg.png'
-            alt='dFlow-logo'
-            width={32}
-            height={32}
-            className='object-contain'
-          />
-          <p className='hidden sm:block'>dFlow</p>
-        </Link>
+    <div className='sticky top-0 z-50 w-full bg-background'>
+      <div className='mx-auto flex w-full max-w-6xl items-center justify-between p-4'>
+        <div className='flex min-h-9 items-center gap-2 text-2xl font-semibold'>
+          <Link href={`/dashboard`} className='flex items-center gap-1'>
+            <Image
+              src='/images/dflow-no-bg.png'
+              alt='dFlow-logo'
+              width={32}
+              height={32}
+              className='object-contain'
+            />
+            <p className='hidden sm:block'>dFlow</p>
+          </Link>
 
-        {/* Breadcrumb placeholders */}
-        <div id='projectName'></div>
-        <div id='serviceName' className='-ml-2'></div>
-        <div id='serverName' className='-ml-4'></div>
-      </div>
+          {/* Breadcrumb placeholders */}
+          <div id='projectName'></div>
+          <div id='serviceName' className='-ml-2'></div>
+          <div id='serverName' className='-ml-4'></div>
+        </div>
 
-      <div className='flex items-center gap-x-4'>
-        <Link
-          className={cn(
-            buttonVariants({
-              variant: 'secondary',
-              size: 'sm',
-            }),
-            'group hidden md:inline-flex',
-          )}
-          target='_blank'
-          href='https://github.com/akhil-naidu/dflow'>
-          <div className='flex items-center'>
-            <Github className='size-4' />
-            <span className='ml-1 hidden md:inline'>Star on GitHub</span>{' '}
-          </div>
-        </Link>
+        <div className='flex items-center gap-x-4'>
+          <Link
+            className={cn(
+              buttonVariants({
+                variant: 'secondary',
+                size: 'sm',
+              }),
+              'group hidden md:inline-flex',
+            )}
+            target='_blank'
+            href='https://github.com/akhil-naidu/dflow'>
+            <div className='flex items-center'>
+              <Github className='size-4' />
+              <span className='ml-1 hidden md:inline'>Star on GitHub</span>{' '}
+            </div>
+          </Link>
 
-        <Suspense fallback={<NavUserSkeleton />}>
-          <NavUserSuspended user={user} />
-        </Suspense>
+          <Suspense fallback={<NavUserSkeleton />}>
+            <NavUserSuspended user={user} />
+          </Suspense>
+        </div>
       </div>
     </div>
   )
