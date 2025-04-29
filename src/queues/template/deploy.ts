c2e91c2edd3a9480eb85c1c90baf931cc6caa407
pavanbhaskar <pavanbhaskardev@gmail.com>
2025-04-29 19:32:04 +0530

    feat: added template deployment queue

--- src/queues/template/deploy.ts
+++ src/queues/template/deploy.ts
@@ -116,28 +116,6 @@ const worker = new Worker<QueueArgs>(
         throw new Error('Please attach services to deploy the template')
       }
 
-      // 0.25 change the naming of services
-      // generate a random-name for project
-      // create a object mapping for old-service name with new-service name
-      // const serviceNames = { old-name: new-name }
-      // change the replace the env with new-name using regex
-      const projectName = handleGenerateName()
-      const serviceNames = {} as Record<string, string>
-
-      services.forEach(service => {
-        const serviceName = handleGenerateName()
-
-        if (service?.name) {
-          serviceNames[service?.name] = `${projectName}-${serviceName}`
-        }
-      })
-
-      const updatedServices = services.map(service => {
-        const serviceName = serviceNames[`${service?.name}`]
-      })
-
-      // 0.5: create a deployment strategy
-
       sendEvent({
         message: 'Started deploying template',
         pub,
@@ -468,8 +446,9 @@ const worker = new Worker<QueueArgs>(
             }
           }
 
+          console.log('outside database', serviceDetails.databaseDetails)
           if (type === 'database' && serviceDetails.databaseDetails?.type) {
-            // add ports exposing process
+            console.log('database hitted')
             const databaseQueueResponse = await addCreateDatabaseQueue({
               databaseName: serviceDetails.name,
               databaseType: serviceDetails.databaseDetails?.type,
