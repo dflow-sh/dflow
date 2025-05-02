eefe40c99c07ee1bdda562dc2b540c20e02fbd4a
pavanbhaskar <pavanbhaskardev@gmail.com>
2025-05-02 17:51:37 +0530

    chore: changed rewrites in nextjs-config for posthog

--- next.config.ts
+++ next.config.ts
@@ -9,8 +9,16 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
+        source: '/ingest/static/:path(.*)',
+        destination: 'https://us-assets.i.posthog.com/static/:path(.*)',
+      },
+      {
         source: '/ingest/:path(.*)',
-        destination: `https://us.i.posthog.com/:path*`,
+        destination: 'https://us.i.posthog.com/:path(.*)',
+      },
+      {
+        source: '/ingest/decide',
+        destination: 'https://us.i.posthog.com/decide',
       },
     ]
   },
