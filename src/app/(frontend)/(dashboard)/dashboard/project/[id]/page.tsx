7372a4cb40c0f42867994ada1146d400d0f6b110
jagadeesh507 <jagadeeshm778@gmail.com>
2025-05-02 12:36:46 +0530

    feat: replace ServicesArchitecture with ServiceList for service visualization

--- src/app/(frontend)/(dashboard)/dashboard/project/[id]/page.tsx
+++ src/app/(frontend)/(dashboard)/dashboard/project/[id]/page.tsx
@@ -5,8 +5,9 @@ import { getPayload } from 'payload'
 import { Suspense, use } from 'react'
 
 import SidebarToggleButton from '@/components/SidebarToggleButton'
-import ServicesArchitecture from '@/components/project/ServicesArchitecture'
 import CreateService from '@/components/service/CreateService'
+import ServiceList from '@/components/service/ServiceList'
+import ServicesArchitecture from '@/components/service/ServicesArchitecture'
 import { ProjectSkeleton } from '@/components/skeletons/ProjectSkeleton'
 import DeployTemplate from '@/components/templates/DeployTemplate'
 import { Service } from '@/payload-types'
@@ -53,26 +54,19 @@ const SuspendedPage = ({ params }: PageProps) => {
         {typeof projectDetails.server === 'object' && (
           <div className='flex items-center gap-3'>
             <DeployTemplate />
-            <CreateService server={projectDetails.server} project={project} />
+            {services?.docs?.length! > 0 && (
+              <CreateService server={projectDetails.server} project={project} />
+            )}
           </div>
         )}
       </div>
       {services?.docs?.length! > 0 ? (
-        <ServicesArchitecture
+        <ServiceList
           projectId={projectDetails.id}
           services={services?.docs as Service[]}
         />
       ) : (
-        <section className='grid min-h-[calc(100vh-40vh)] w-full place-items-center'>
-          <div className='relative mx-auto w-full max-w-lg rounded-lg border border-slate-800 bg-gradient-to-tr from-slate-800 to-slate-900 px-8 pb-6 pt-8 text-center'>
-            <h4 className='inline-flex bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 bg-clip-text text-2xl font-bold text-transparent'>
-              No Services Found
-            </h4>
-            <p className='text-cq-text-secondary text-pretty pt-1'>
-              You don't have any services yet. Start by creating a new service
-            </p>
-          </div>
-        </section>
+        <ServicesArchitecture />
       )}
     </section>
   )
