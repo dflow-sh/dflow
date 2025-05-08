806d10f036c6bf0ea9b070424aff4bd2d455f95f
jagadeesh507 <jagadeeshm778@gmail.com>
2025-05-08 12:43:04 +0530

    feat: implement getTenant function for tenant retrieval and validation

---
+++ src/lib/get-tenant.ts
@@ -0,0 +1,46 @@
+// utils/getTenant.ts
+import configPromise from '@payload-config'
+import { cookies, headers } from 'next/headers'
+import { forbidden } from 'next/navigation'
+import { getPayload } from 'payload'
+
+import { Tenant } from '@/payload-types'
+
+import { getCurrentUser } from './getCurrentUser'
+
+type UserTenant = {
+  tenant: Tenant
+  roles: ('tenant-admin' | 'tenant-user')[]
+}
+export async function getTenant() {
+  const headersList = await headers()
+  const cookieStore = await cookies()
+  const slug = cookieStore.get('organisation')?.value
+
+  if (!slug) throw new Error('Organisation slug not found in cookie')
+
+  const user = await getCurrentUser(headersList)
+  const payload = await getPayload({ config: configPromise })
+
+  const { docs } = await payload.find({
+    collection: 'tenants',
+    where: { slug: { equals: slug } },
+  })
+
+  const tenant = docs[0]
+
+  if (!tenant) forbidden()
+
+  const matchedTenantEntry = user?.tenants?.find(entry => {
+    const tenantId =
+      typeof entry.tenant === 'string' ? entry.tenant : entry.tenant.id
+    return tenantId === tenant.id
+  })
+
+  if (!Boolean(matchedTenantEntry)) forbidden()
+  return {
+    user,
+    userTenant: matchedTenantEntry as UserTenant,
+    isInTenant: Boolean(matchedTenantEntry),
+  }
+}
