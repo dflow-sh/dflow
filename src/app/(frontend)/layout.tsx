730e6bd706c1889d22f74b6c0a63524cd5865da3
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2025-06-02 23:02:03 +0530

    feat: integrate Google Tag Manager for enhanced tracking and analytics

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from 'next/font/google'
+import Script from 'next/script'
 import React from 'react'
 import { Toaster } from 'sonner'
 
@@ -38,6 +39,20 @@ export default async function RootLayout({
   return (
     <html lang='en' suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <Script
+          id='gtm-script'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NH2BJRRR');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         {/* Added react-scan for fixing performance pit-holes */}
         {/* {process.env.NODE_ENV === 'development' && (
           <script
@@ -48,6 +63,17 @@ export default async function RootLayout({
         )} */}
       </head>
       <body className={`${geistSans.className} ${geistMono.variable}`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src='https://www.googletagmanager.com/ns.html?id=GTM-NH2BJRRR'
+            height='0'
+            width='0'
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <NProgressProvider>
           <PosthogProvider>
             <SuspendedPostHogPageView />
