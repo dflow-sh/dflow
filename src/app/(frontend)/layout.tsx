f580790425181f6379d36829ec1ff421e4bd0105
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2025-06-02 23:06:16 +0530

    refactor: replace Script component with native script tag for Google Tag Manager

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,5 +1,4 @@
 import { Geist, Geist_Mono } from 'next/font/google'
-import Script from 'next/script'
 import React from 'react'
 import { Toaster } from 'sonner'
 
@@ -40,9 +39,7 @@ export default async function RootLayout({
     <html lang='en' suppressHydrationWarning>
       <head>
         {/* Google Tag Manager */}
-        <Script
-          id='gtm-script'
-          strategy='afterInteractive'
+        <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
