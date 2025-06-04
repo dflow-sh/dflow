8cb9152ec5e0c16903edd4652b2c31e85c08e244
jagadeesh507 <jagadeeshm778@gmail.com>
2025-06-04 20:58:29 +0530

    refactor: update template grid layout and enhance TemplateCard and TemplateDetails components

--- src/components/templates/TemplateCard.tsx
+++ src/components/templates/TemplateCard.tsx
@@ -6,31 +6,35 @@ import { Template } from '@/payload-types'
 
 const TemplateCard = ({ template }: { template: Template }) => {
   return (
-    <Card>
-      <CardContent className='p-6'>
-        <Image
-          unoptimized
-          alt='Template Image'
-          src={template?.imageUrl || '/images/favicon.ico'}
-          className='h-10 w-10 rounded-md'
-          width={100}
-          height={100}
-        />
+    <div>
+      <Card>
+        <CardContent className='flex h-56 flex-col justify-between p-6'>
+          <div>
+            <Image
+              unoptimized
+              alt='Template Image'
+              src={template?.imageUrl || '/images/favicon.ico'}
+              className='h-10 w-10 rounded-md'
+              width={100}
+              height={100}
+            />
 
-        <div className='mt-4 flex flex-col gap-1'>
-          <p className='line-clamp-1 text-lg font-semibold'>{template?.name}</p>
-          <p className='line-clamp-2 text-sm text-muted-foreground'>
-            {template?.description}
-          </p>
-        </div>
+            <div className='mt-4 space-y-1'>
+              <p className='line-clamp-1 text-lg font-semibold'>
+                {template?.name}
+              </p>
+              <p className='line-clamp-2 text-sm text-muted-foreground'>
+                {template?.description}
+              </p>
+            </div>
+          </div>
 
-        <div className='mt-6 flex justify-end'>
-          <Button disabled variant='outline'>
-            Deploy
-          </Button>
-        </div>
-      </CardContent>
-    </Card>
+          <div className='mt-6 flex items-end justify-end'>
+            <Button variant='outline'>Deploy</Button>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   )
 }
 
--- src/components/templates/TemplateDetails.tsx
+++ src/components/templates/TemplateDetails.tsx
@@ -24,21 +24,25 @@ const TemplateDetails = ({ template }: { template: Template }) => {
 
   return (
     <Card>
-      <CardContent className='p-6'>
-        <img
-          alt='Template Image'
-          src={template?.imageUrl || '/images/favicon.ico'}
-          className='h-10 w-10 rounded-md'
-        />
+      <CardContent className='flex h-56 flex-col justify-between p-6'>
+        <div>
+          <img
+            alt='Template Image'
+            src={template?.imageUrl || '/images/favicon.ico'}
+            className='h-10 w-10 rounded-md'
+          />
 
-        <div className='mt-4 flex flex-col gap-1'>
-          <p className='line-clamp-1 text-lg font-semibold'>{template.name}</p>
-          <p className='line-clamp-2 text-sm text-muted-foreground'>
-            {template.description}
-          </p>
+          <div className='mt-4 space-y-1'>
+            <p className='line-clamp-1 text-lg font-semibold'>
+              {template.name}
+            </p>
+            <p className='line-clamp-2 text-sm text-muted-foreground'>
+              {template.description}
+            </p>
+          </div>
         </div>
 
-        <div className='mt-6 flex justify-end gap-3'>
+        <div className='mt-6 flex items-end justify-end gap-3'>
           <Link
             href={`/${(template?.tenant as Tenant)?.slug}/templates/compose?templateId=${template?.id}`}>
             <Button variant={'outline'}>
