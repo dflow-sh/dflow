ff34cdc7d15953a4c8d2104400a334a4ad38d5bf
lokesh mvs <lokeshmvs21@gmail.com>
2025-06-13 09:57:09 +0530

    chore: Added start date and end date functionality for banners

--- src/actions/banners/index.ts
+++ src/actions/banners/index.ts
@@ -7,27 +7,31 @@ export const getAllBanners = protectedClient
   .action(async ({ ctx }) => {
     const { payload, userTenant } = ctx
 
+    const now = new Date().toISOString()
+
     const { docs: banners } = await payload.find({
       collection: 'banners',
       pagination: false,
       where: {
-        or: [
-          {
-            scope: {
-              equals: 'global',
-            },
-          },
-          {
-            'tenant.slug': {
-              equals: userTenant.tenant?.slug,
-            },
-          },
-        ],
         and: [
           {
-            isActive: {
-              equals: true,
-            },
+            or: [
+              { scope: { equals: 'global' } },
+              { 'tenant.slug': { equals: userTenant.tenant?.slug } },
+            ],
+          },
+          { isActive: { equals: true } },
+          {
+            or: [
+              { startDate: { equals: null } },
+              { startDate: { less_than_equal: now } },
+            ],
+          },
+          {
+            or: [
+              { endDate: { equals: null } },
+              { endDate: { greater_than_equal: now } },
+            ],
           },
         ],
       },
