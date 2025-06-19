9123d1d09a7fe9cffda07a2d979a59f99bf93285
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2025-06-19 11:20:09 +0530

    chore: updated cloud-init

--- scripts/cloud-init.sh
+++ scripts/cloud-init.sh
@@ -2,9 +2,6 @@
 
 set -e  # Exit on error
 
-# Log everything
-exec > >(tee /var/log/dflow-bootstrap.log | logger -t user-data -s 2>/dev/console) 2>&1
-
 echo "[+] Installing Dokku v0.35.20"
 wget -q https://dokku.com/bootstrap.sh
 DOKKU_TAG="v0.35.20"
