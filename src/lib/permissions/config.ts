4f2915390b7ae833e5f5b12adec244cb3024bcf0
pavanbhaskar <pavanbhaskardev@gmail.com>
2025-08-04 14:41:56 +0530

    chore: converted protected-client to user-client for impersonate action

--- src/lib/permissions/config.ts
+++ src/lib/permissions/config.ts
@@ -207,9 +207,6 @@ export const getActionAccess = {
   startTerminalAction: ['servers.read'],
   stopTerminalAction: ['servers.read'],
   restartTerminalAction: ['servers.read'],
-
-  // Impersonate Action
-  impersonateUserAction: [],
 } as const
 
 export type GetActionAccessMap = typeof getActionAccess
