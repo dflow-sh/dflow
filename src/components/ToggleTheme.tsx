fdc6ea2f1fcb6dde688ae32ecdce7695284fbfb6
pavanbhaskar <pavanbhaskardev@gmail.com>
2025-08-08 16:00:29 +0530

    chore: updated default color-palate

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -18,14 +18,13 @@ export default function ToggleTheme() {
     <Toggle
       variant='outline'
       className={
-        'group hidden size-9 data-[state=on]:bg-transparent data-[state=on]:hover:bg-muted md:block'
+        'group hidden size-9 data-[state=on]:bg-transparent data-[state=on]:hover:bg-muted md:inline-flex'
       }
       pressed={theme === 'dark'}
       onPressedChange={() =>
         setTheme(prev => (prev === 'dark' ? 'light' : 'dark'))
       }
       aria-label={`Switch to ${theme === 'dark' ? 'light' : 'dark'} mode`}>
-      {/* Use Tailwind's dark: and data-[state=on]: to control icon visibility */}
       <MoonIcon
         size={16}
         className='shrink-0 scale-0 opacity-0 transition-all data-[state=on]:scale-100 data-[state=on]:opacity-100 dark:scale-100 dark:opacity-100'
