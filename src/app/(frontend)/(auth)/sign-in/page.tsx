3fad7620484af0469bd65eed94f39ef1badc7c4a
jagadeesh507 <jagadeeshm778@gmail.com>
2025-10-07 13:08:59 +0530

    fix: redirect to main site for app.dflow.sh in sign-in and sign-up pages

--- src/app/(frontend)/(auth)/sign-in/page.tsx
+++ src/app/(frontend)/(auth)/sign-in/page.tsx
@@ -1,10 +1,20 @@
 import { env } from 'env'
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
 
 import { getAuthConfigAction } from '@/actions/pages/auth'
 import SignInForm from '@/components/sign-in/SignInForm'
+import { DFLOW_CONFIG } from '@/lib/constants'
 import { AuthConfig } from '@/payload-types'
 
 const SignInPage = async () => {
+  const headersList = await headers()
+  const host = headersList.get('host') || ''
+  const redirectionURL = DFLOW_CONFIG.URL
+  if (host === 'app.dflow.sh') {
+    redirect(`${redirectionURL}/sign-in`)
+  }
+
   const result = await getAuthConfigAction()
   const authMethod = (result?.data?.authConfig.authMethod ||
     'both') as AuthConfig['authMethod']
